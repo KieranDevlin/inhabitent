77f5599db07d91970d39788db46870452d043f6b
Kieran Devlin <kjrdevlin@gmail.com>
2019-11-07 16:23:11 -0800

    initial commit

---
+++ wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1573776734;s:7:"authKey";s:64:"ZJ${rI)z1Hy{2C?ShCHb57N{/ZOIz9x!5F. ?v>iFur4j1lx93iml/n2`LAn %UO";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2502;}
