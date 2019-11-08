5c61814455152fbc74d904b133e1027525ebbe38
Kieran Devlin <kjrdevlin@gmail.com>
2019-11-08 13:36:38 -0800

    update variables

---
+++ db.php
@@ -0,0 +1,1 @@
+/Users/kieranandcammy/Desktop/Kieran Devlin/MAMP Projects/inhabitent/wp-content/plugins/query-monitor/wp-content/db.php
--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Custom functions that act independently of the theme templates.
  *
@@ -11,12 +12,36 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function red_starter_body_classes( $classes ) {
+function red_starter_body_classes($classes)
+{
 	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
+	if (is_multi_author()) {
 		$classes[] = 'group-blog';
 	}
 
 	return $classes;
 }
-add_filter( 'body_class', 'red_starter_body_classes' );
+add_filter('body_class', 'red_starter_body_classes');
+
+function red_starter_login_logo()
+{ ?>
+
+	<style type="text/css">
+		body.login div#login h1 a {
+			background-image: url("<?= get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-tent.svg");
+			padding-bottom: 30px;
+
+
+		}
+	</style>
+<?php }
+
+add_action('login_enqueue_scripts', 'red_starter_login_logo');
+add_filter('login_headerurl', 'red_starter_logo_url');
+
+function red_starter_logo_url($url)
+{
+	return get_home_url('url');
+}
+
+?>
