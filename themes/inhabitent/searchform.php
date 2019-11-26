bc83134834bc2cb55bbb49ce04e81bb01bac6355
Kieran Devlin <kjrdevlin@gmail.com>
2019-11-26 11:11:50 -0800

    update label and input in searchform.php

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,7 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
 	<fieldset>
-		<label for="search-input" aria-label="Seach"></label>
-		<input type="search" id="search-input" class="search-field" placeholder="TYPE AND HIT ENTER ..." value="<?= esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
-		</label>
+		<label for="search-input" aria-label="Seach">TYPE AND HIT ENTER ...</label>
+		<input type="search" id="search-input" class="search-field" value="<?= esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
 	</fieldset>
 </form>
