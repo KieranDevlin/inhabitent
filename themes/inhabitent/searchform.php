3f5cb5477d45023bd34d5ddb5007ace240939e08
Kieran Devlin <kjrdevlin@gmail.com>
2019-11-26 11:08:42 -0800

    fix typo in searchform.php label

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
 	<fieldset>
-		<label for="search-input" aria-label="Seach"></labe>
-			<input type="search" id="search-input" class="search-field" placeholder="TYPE AND HIT ENTER ..." value="<?= esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
+		<label for="search-input" aria-label="Seach"></label>
+		<input type="search" id="search-input" class="search-field" placeholder="TYPE AND HIT ENTER ..." value="<?= esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
 		</label>
 	</fieldset>
 </form>
